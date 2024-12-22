961ebc55f8809dbb784f34328fca8e80b92e0163
AHpxMac <ahpx@yandex.com>
2024-12-22 16:32:05 +0800

    clean: clean redundant files and config

--- src/manifest.ts
+++ src/manifest.ts
@@ -19,20 +19,9 @@ export default defineManifest({
     default_popup: 'popup.html',
     default_icon: 'img/logo-48.png',
   },
-  options_page: 'options.html',
-  devtools_page: 'devtools.html',
   background: {
     service_worker: 'src/background/index.ts',
     type: 'module',
-  },
-  content_scripts: [
-    {
-      matches: ['http://*/*', 'https://*/*'],
-      js: ['src/contentScript/index.ts'],
-    },
-  ],
-  side_panel: {
-    default_path: 'sidepanel.html',
   },
   web_accessible_resources: [
     {
@@ -40,8 +29,5 @@ export default defineManifest({
       matches: [],
     },
   ],
-  permissions: ['sidePanel', 'storage'],
-  chrome_url_overrides: {
-    newtab: 'newtab.html',
-  },
+  permissions: ['sidePanel', 'storage']
 })
